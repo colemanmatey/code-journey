adb60e5144546dc7591e3ce1fe5fcf1c8d6ce1ac
Coleman A. Matey <113218526+colemanmatey@users.noreply.github.com>
2023-12-08 13:35:39 +0000

    Learn how to use properties

---
+++ tutorial/classes/properties.py
@@ -0,0 +1,23 @@
+"""Getters and Setters"""
+
+
+class Person:
+
+    def __init__(self, name, age):
+        self._name = name
+        self._age = age
+
+    @property
+    def name(self):
+        return self._name * 3
+    
+    @name.setter
+    def name(self, value):
+        self._name = value.upper()
+        raise Exception("Cannot change name!")
+
+person = Person("Cole", 28)
+print(person.name)
+
+person.name = "Mildred"
+print(person.name)

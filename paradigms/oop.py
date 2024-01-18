b85b51f62d017a7858ebe823d25c4ea88f4b974f
Coleman A. Matey <113218526+colemanmatey@users.noreply.github.com>
2024-01-18 14:04:09 +0000

    Create oop.py

---
+++ paradigms/oop.py
@@ -0,0 +1,12 @@
+"""
+Object-Oriented Programming (OOP) is a programming paradigm that organizes code 
+into reusable units called "objects," which encapsulate data and behavior. 
+In OOP, these objects are instances of classes, which are blueprints defining 
+the properties (attributes) and actions (methods) that the objects can have. 
+OOP promotes concepts such as encapsulation, inheritance, and polymorphism, 
+allowing for modular and more easily maintainable code.
+"""
+
+
+class Book:
+    pass

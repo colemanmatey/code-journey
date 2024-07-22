e9d238b419a815e413b7ca2e221ea163bcd71056
Coleman Matey <113218526+colemanmatey@users.noreply.github.com>
2024-07-22 17:01:24 -0400

    Add database project

---
+++ LearnSSDT/schema/tables/Article.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Article]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Title] VARCHAR(50) NULL, 
+    [Content] TEXT NULL, 
+    [AuthorID] INT NOT NULL, 
+    CONSTRAINT [FK_Article_Author] FOREIGN KEY (AuthorID) REFERENCES [Author]([Id])
+)
---
+++ LearnSSDT/schema/tables/Author.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Author]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [FirstName] NVARCHAR(50) NOT NULL, 
+    [LastName] NVARCHAR(50) NOT NULL, 
+    [OtherNames] NVARCHAR(100) NULL, 
+    [Bio] TEXT NULL
+)

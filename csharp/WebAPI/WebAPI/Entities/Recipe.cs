4af19952b99598bb2aa2bbfd8b181530fe05909b
Coleman Matey <113218526+colemanmatey@users.noreply.github.com>
2025-05-22 12:04:47 -0400

    Remove WeatherForecast model and controller

---
+++ csharp/WebAPI/WebAPI/Entities/Recipe.cs
@@ -0,0 +1,10 @@
+﻿namespace WebAPI.Entities
+{
+    public class Recipe
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public List<Ingredient>? Ingredients { get; set; }
+        public string? Instructions { get; set; }
+    }
+}
